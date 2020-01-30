428c18f9e40c11faf3e5b6c64f135dc411c09e86
Esteban Zapata <esteban.zapata@nebulae.com.co>
2020-01-30 16:21:11 -0500

    Update script to allow create orderboxes

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ const MongoClient = require('mongodb').MongoClient;
 const urlCurrent = 'mongodb://localhost:27017';
 const urlHistory = 'mongodb://localhost:27018';
 const COLLECTION_NAME = 'Orderbox';
-const dbNameHistory = 'historical_1910';
+const dbNameHistory = 'historical_2001';
 const dbNameCurrent = 'logistic-operation';
+
+const PASSED_AUDIT_ENU = {
+    0: "NOT_REQUIRED",
+    1: "COMPLETED_AUDIT",
+    2: "NOT_COMPLETED_AUDIT",
+    3: "NOT_INVENTORY"
+  }
 
 MongoClient.connect(urlHistory, function(err, dbHistory) {
 
@@ -34,13 +41,19 @@ MongoClient.connect(urlHistory, function(err, dbHistory) {
 
 const start = (mongoDBHistory, mongoDBCurrent) => {
     const query = {
-        'retrievalId': 'c91e13ef-8726-4b03-9a41-a171045fdca6'
+        //'retrievalId': 'c91e13ef-8726-4b03-9a41-a171045fdca6'
+        'lastModificationTimestamp': { $gte: 1580274000000 },
+        'evicted': true
+        //_id: '3d3a0b60-f7ea-4084-b80d-58ce46a40326-2001'
     };
-    defer(() => mongoDBHistory.collection(COLLECTION_NAME).find(query).toArray())
+    defer(() => mongoDBCurrent.collection(COLLECTION_NAME).find(query).toArray())
     .pipe(
+        tap(result => {
+            console.log(result.length);
+        }),
         mergeMap(orderboxes => from(orderboxes)),
         mergeMap(orderbox => {
-
+            //console.log('orderbox => ', orderbox);
             const updateOperation = {
                 stateChanges: orderbox.stateChanges,
                 currentState: orderbox.currentState,
@@ -57,10 +70,36 @@ const start = (mongoDBHistory, mongoDBCurrent) => {
                 distributionCenterName: orderbox.distributionCenterName,
                 expirationTimestamp: orderbox.expirationTimestamp,
                 promiseTime: orderbox.promiseTime,
-                //auditAnswer: orderbox.auditAnswer,
+                auditAnswer: PASSED_AUDIT_ENU[orderbox.auditAnswer || 0],
                 stowageName: orderbox.stowageName,
                 stowageId: orderbox.stowageId,
                 deliveryId: orderbox.deliveryId,
+                contentQty: orderbox.contentQty,
+                audit: orderbox.audit,
+                barcode: orderbox.barcode,
+                brand: orderbox.brand,            
+                businessId: orderbox.businessId,
+                campaign: orderbox.campaign,
+                contentDesc: orderbox.contentDesc,
+                generatorBoxId: orderbox.generatorBoxId,
+                generatorDispatch: orderbox.generatorDispatch,
+                generatorDispatchId: orderbox.generatorDispatchId,
+                generatorDocumentId: orderbox.generatorDocumentId,
+                generatorExtras: orderbox.generatorExtras,
+                generatorId: orderbox.generatorId,
+                generatorInvoiceId: orderbox.generatorInvoiceId,
+                generatorName: orderbox.generatorName,
+                generatorOrderId: orderbox.generatorOrderId,
+                logisticOperatorCode: orderbox.logisticOperatorCode,
+                logisticOperatorId: orderbox.logisticOperatorId,
+                logisticOperatorName: orderbox.logisticOperatorName,
+                orderId: orderbox.orderId,
+                orderboxesCount: orderbox.orderboxesCount,
+                productType: orderbox.productType,
+                shopper: orderbox.shopper,
+                deliveryLicensePlate: orderbox.deliveryLicensePlate,
+                retrievalId: orderbox.retrievalId,
+                timestamp: orderbox.timestamp
             };
 
             if(orderbox.deliveryProof){
@@ -71,8 +110,8 @@ const start = (mongoDBHistory, mongoDBCurrent) => {
                 updateOperation['deliveries'] = orderbox.deliveries;
             }
 
-            return defer(() => mongoDBCurrent.collection(COLLECTION_NAME).findOneAndUpdate({_id: orderbox._id}, {$set: updateOperation}, 
-            { returnOriginal: false }))
+            return defer(() => mongoDBHistory.collection(COLLECTION_NAME).findOneAndUpdate({_id: orderbox._id}, {$set: updateOperation}, 
+            { returnOriginal: false, upsert: true }))
             .pipe(
                 map(result => result && result.value ? result.value : undefined)
             )
@@ -80,7 +119,7 @@ const start = (mongoDBHistory, mongoDBCurrent) => {
 
     )
     .subscribe(result => {
-        console.log('Result => ', result);
+        //console.log('Result => ', result);
     }, error => {
         console.log('Error subscription => ', error);
     },
